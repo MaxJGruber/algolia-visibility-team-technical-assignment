bedd88e03dc66f2c97c73e903cc6162921acc97e
MaxJGruber <maxjgruber@gmail.com>
2023-12-22 13:50:00 +0100

    Add some next-seo

--- config.ts
+++ config.ts
@@ -1,3 +1,6 @@
 export const BASE_URL = "https://jsonplaceholder.typicode.com";
 
 export const ALL_USERS_ENDPOINT = "/users";
+
+export const TITLE = "Max Gruber Algolia Technical Assignment";
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 import AppLayout from "@/layouts/AppLayout";
+import { TITLE } from "@/config";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <DefaultSeo title={TITLE} />
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </>
   );
 }
